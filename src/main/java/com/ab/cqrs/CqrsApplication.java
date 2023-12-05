a06cab1acbb10e51e7b9cc3b0c9ebac038354504
Hassan ABIDA <abidahass.uca@gmail.com>
2023-12-05 16:36:27 +0100

    Init project

---
+++ src/main/java/com/ab/cqrs/CqrsApplication.java
@@ -0,0 +1,13 @@
+package com.ab.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsApplication.class, args);
+	}
+
+}
